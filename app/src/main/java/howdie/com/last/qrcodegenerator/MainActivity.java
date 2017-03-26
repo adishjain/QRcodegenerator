1d29210a7685584745f2242b3c3681a1aef9cb07
adishjain <adish.jain1995@ymail.com>
2017-03-26 06:18:38 +0530

    Ip and ports added

--- app/src/main/java/howdie/com/last/qrcodegenerator/MainActivity.java
+++ app/src/main/java/howdie/com/last/qrcodegenerator/MainActivity.java
@@ -1,8 +1,10 @@
 package howdie.com.last.qrcodegenerator;
 
 import android.graphics.Bitmap;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
@@ -16,13 +18,19 @@ import static android.graphics.Color.WHITE;
 
 public class MainActivity extends AppCompatActivity {
 
+    String ip, dataPort, controlPort, data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView imageView = (ImageView) findViewById(R.id.qrcode);
+        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        dataPort = "8888";
+        controlPort = "8080";
+        data=ip+","+dataPort+","+controlPort;
         try {
-            Bitmap bitmap = encodeAsBitmap("HELLO");
+            Bitmap bitmap = encodeAsBitmap(data);
             imageView.setImageBitmap(bitmap);
         } catch (WriterException e) {
             e.printStackTrace();
